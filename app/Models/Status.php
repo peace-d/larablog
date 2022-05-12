48dbe1600c25e49295953315339b9db8b83c7d00
Nkululeko Dube <nkululeko.d@afrihost.com>
2022-05-12 16:29:34 +0200

    add status const

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    public const STATUS_ACTIVE_ID = 1;
+    public const STATUS_PENDING_ID = 2;
+    public const STATUS_DRAFT_ID = 3;
+    public const STATUS_DELETE_ID = 4;
+
     use HasFactory;
 }
