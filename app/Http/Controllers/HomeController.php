76385fba3837a548d4729750e0740f78415371ed
Nkululeko Dube <nkululeko.d@afrihost.com>
2022-05-12 23:43:42 +0200

    move dashboard view to controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Status;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +17,6 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
     }
 
     /**
@@ -23,6 +26,21 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $this->middleware('auth');
         return view('home');
+    }
+
+    public function dashboard()
+    {
+        $loggedUser = [];
+        if (auth()->user()) {
+            $loggedUser = User::findOrFail(auth()->user()->id);
+        }
+
+        return view('welcome', [
+            'posts' => Post::where( ['status_id' => Status::STATUS_ACTIVE_ID] )->get(),
+            'categories' => Category::all(),
+            'loggedUser' => $loggedUser
+        ]);
     }
 }
